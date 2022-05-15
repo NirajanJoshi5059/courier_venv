094992932406cf37a5d102a9a5d2980646342b26
NirajanJoc <Nirajanjoc19@gmail.com>
2022-05-15 22:50:19 +0545

    'migration'

--- demoapp/models.py
+++ demoapp/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name= models.CharField(max_length=255, unique=True)
+    price=models.CharField(max_length=255)
+    category=models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+ 
